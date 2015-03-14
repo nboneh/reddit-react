994752653e33f7fe78d03acdcc642a6e79ae7e2b
Nir Boneh <nboneh5921@gmail.com>
2015-03-14 13:26:17 -0600

    Getting Started

---
+++ README.md
@@ -0,0 +1,12 @@
+# reddit-react
+
+Reddit Frontend powered by ReactJS
+
+
+# Deploy
+
+    $ wintersmith build
+
+    $ git commit -a -m 'build'
+
+    $ git subtree push --prefix build origin gh-pages
---
+++ contents/js/app.js
@@ -0,0 +1,14 @@
+$( "#hotList" ).on( "click", function( event ) {
+    React.render(
+        <List url='https://api.reddit.com/hot'/>,
+        document.getElementById('content')
+    )
+})
+
+$( "#newList" ).on( "click", function( event ) {
+     React.render(
+        <List url='https://api.reddit.com/new'/>,
+        document.getElementById('content')
+    )
+})
+
