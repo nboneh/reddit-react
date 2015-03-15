e4aa5b2b513e16c3df4bf2bc903198482a5d769d
Nir Boneh <nboneh5921@gmail.com>
2015-03-14 21:32:40 -0600

    Configuring react variables

---
+++ build/js/listing/list.js
@@ -0,0 +1,50 @@
+var List = React.createClass({displayName: 'List',
+
+    getInitialState: function() {
+        return {data: []};
+    },
+  
+    componentDidMount: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({
+                    data: data.data.children
+                });
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    handleListItemClicked: function(id){
+        var post = _.find(this.state.data, {data.id: id})
+        alert(post);
+        this.refs.postiew.setState({post:post})
+    },
+
+    render: function() {
+
+        var self = this
+
+        var posts = this.state.data.map(function (post) {
+        
+          return (    
+            <ListItem post={post} onListItemClicked={self.handleListItemClicked}/>
+            )
+        })        
+
+        return (
+            <div className="postPage">
+                <div className="list six columns">
+                    {posts}
+                </div>
+                <div className="post six columns">
+                    <Post ref="post"/>
+                </div>
+            </div>
+        )
+    }
+})
