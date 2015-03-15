a1ab10564fb4969c7c423b894d7058f960314e29
Nir Boneh <nboneh5921@gmail.com>
2015-03-15 12:54:34 -0600

    Done 50/50

--- js/listing/post.js
+++ js/listing/post.js
@@ -1,25 +1,26 @@
 var Post = React.createClass({
 
     getInitialState: function() {
-        return {post: null}
+        return {url: null, title: null}
     },
 
     render: function() {
         
-        if (this.state.post){
+        if (this.state.title && this.state.url){
 
             return ( 
                 <div className="post" >
-                    <h2 className="postTitle">
-                        {this.state.post.data.title}
-                    </h2>
+                    <h5 className="postTitle">
+                        {this.state.title}
+                    </h5>
+                    <iframe src={this.state.url} width='600' height='600' scrolling='yes' frameborder='2'></iframe>
                 </div>
             )
 
         }else{
 
             return (
-                <h2>Click an item to see details</h2>
+                <h2>Click title to see details</h2>
             )
         }
   }
